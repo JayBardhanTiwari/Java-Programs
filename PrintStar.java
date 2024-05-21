b3bfbc75512c1b54933a86a9a0e856dd2468107e
Jay Bardhan Tiwari <103141320+JayBardhanTiwari2000@users.noreply.github.com>
2024-05-22 03:07:13 +0530

    edited code

--- PrintStar.java
+++ PrintStar.java
@@ -1,14 +1,10 @@
 public class PrintStar {
     public static void main(String[] args) {
-        public class PrintStar {
-            public static void main(String[] args) {
-                for (int i = 1; i <=5; i++) {
-                    for (int j = 1; j <= i; j++) {
-                        System.out.print("*");
-                    }
-                    System.out.println();
-                }
+        for (int i = 1; i <=5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
+            System.out.println();
         }
     }
 }
