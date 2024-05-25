fc9eedbfaf97156c440e0520e15503cc18c42cb0
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-05-25 21:25:58 +0530

    edited

--- PrintNumberwithGap.java
+++ PrintNumberwithGap.java
@@ -1,13 +1,32 @@
 public class PrintNumberwithGap {
     public static void main(String[] args) {
-        int num=10;
-        
-        while (i < 10) {
 
-            i++;
+        int num = 1;
+        int total = 20;
+        /*
+         * while (num <= total) {
+         * System.out.print(num);
+         * System.out.println();
+         * // num += 3;
+         * num = num + 3;
+         */
+        // }
+        for ( ; num <= total; ) {
+            System.out.print(num);
+            System.out.println();
+             num = num + 3;
 
-            
         }
+        // for(int num=1;num<=total;num=num+3){
+        //     System.out.println(num);
+        //     System.out.println();
+        //  } 
+
+        
+        //   for(int num=1;num<=total;num+=3){
+        //     System.out.println(num);
+        //     System.out.println();
+         
     }
-    
+
 }
