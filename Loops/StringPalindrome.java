184254cb723a9b3675d91c2a81443f01466d1c09
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-05-29 11:43:09 +0530

    edited

--- Loops/StringPalindrome.java
+++ Loops/StringPalindrome.java
@@ -6,16 +6,17 @@ public class StringPalindrome {
     // Main driver method
     public static void main(String args[]) {
         // 012345
-        String name = "level";
+        String name = "frank";
         String str = "";
 
         for (int i = name.length() - 1; i >= 0; i--) {
             char a = name.charAt(i);
+       
             str = str + a;
 
         }
-        System.out.println(str);
-        System.out.println(name);
+        //System.out.println(str);
+        //System.out.println(name);
         if (name.equals(str)) {
             System.out.println("its palindrome");
         } else {
