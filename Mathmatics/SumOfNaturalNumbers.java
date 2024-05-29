4345fa2bc00edceccb47277f0636791c349e8c23
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-05-29 12:08:57 +0530

    edited

--- Mathmatics/SumOfNaturalNumbers.java
+++ Mathmatics/SumOfNaturalNumbers.java
@@ -3,7 +3,7 @@ package Mathmatics;
 public class SumOfNaturalNumbers {
     public static void main(String[] args) {
         
-        int num= 5;
+        int num= 4;
         int sum = 0;
         int add = 0;
         for(int i=0;i<num;i++){
