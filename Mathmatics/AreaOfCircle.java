1c1d06a17d837a11ee5784545530063b7866d503
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-05-30 11:30:29 +0530

    AreaOfCircle

---
+++ Mathmatics/AreaOfCircle.java
@@ -0,0 +1,12 @@
+package Mathmatics;
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        
+        float radius = 4;
+        double areaofcircle= 0;
+
+        areaofcircle = 3.14 * (radius * radius) ;
+        System.out.println(areaofcircle);
+    }
+
+}
