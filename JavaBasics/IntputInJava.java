d5076633d8868516a4ba35af7711c1a41c563d8e
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-05-31 11:07:49 +0530

    How to take inputs in java

--- JavaBasics/IntputInJava.java
+++ JavaBasics/IntputInJava.java
@@ -1,36 +1,40 @@
 package JavaBasics;
 
-import java.util.Scanner;
+import java.util.*;// Importing a package  named util
 
 public class IntputInJava {
     public static void main(String[] args) {
-        //First we have to make Scanner class
-        Scanner sc = new Scanner(System.in);
-        
-             String input = sc.next(); // Now its stores a single word input
-              System.out.println(input);
 
-            String name = sc.nextLine();// nextline stores can Names,Paragraphs,Sentences
-            System.out.println(name);
+        Scanner sc = new Scanner(System.in); 
+        String input = sc.next(); // Now its stores a single word input
 
-            int number = sc.nextInt();//It takes numbers as Input
-            System.out.println(number);
+        /*
+         Creating a object and
+           for taking input we have to write System.in
+         */
 
-            float decimal = sc.nextFloat();//It takes float values as Input
-            System.out.println(decimal);
+        System.out.println(input);
 
-            double price = sc.nextDouble();//It takes double values as Input
-            System.out.println(price);
+        String name = sc.nextLine();// nextline can stores Names,Paragraphs,Sentences
+        System.out.println(name);
 
-            long big= sc.nextLong();//It takes long values as Input
-            System.out.println(big);
+        int number = sc.nextInt();// It takes numbers as Input
+        System.out.println(number);
 
-            boolean b = sc.nextBoolean();//It takes boolean values as Input
-            System.out.println(b);
+        float decimal = sc.nextFloat();// It takes float values as Input
+        System.out.println(decimal);
 
-            short n= sc.nextShort();//It takes short values as Input
-            System.out.println(n);
+        double price = sc.nextDouble();// It takes double values as Input
+        System.out.println(price);
 
+        long big = sc.nextLong();// It takes long values as Input
+        System.out.println(big);
+
+        boolean b = sc.nextBoolean();// It takes boolean values as Input
+        System.out.println(b);
+
+        short n = sc.nextShort();// It takes short values as Input
+        System.out.println(n);
 
     }
 }
