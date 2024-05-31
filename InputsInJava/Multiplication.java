306b1c18d2592a8a034eac747446e49e9405bc3f
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-05-31 11:39:39 +0530

    Multiplication

---
+++ InputsInJava/Multiplication.java
@@ -0,0 +1,15 @@
+package InputsInJava;
+
+import java.util.Scanner;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int multiply = a * b ;
+        System.out.println(multiply);
+    }
+
+}
