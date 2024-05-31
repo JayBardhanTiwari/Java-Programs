23653f6942ed89fc2a6e343c3c01ba26cd6ebf94
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-05-31 10:59:25 +0530

    Inputs in java

--- JavaBasics/IntputInJava.java
+++ JavaBasics/IntputInJava.java
@@ -6,7 +6,31 @@ public class IntputInJava {
     public static void main(String[] args) {
         //First we have to make Scanner class
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
+        
+             String input = sc.next(); // Now its stores a single word input
+              System.out.println(input);
+
+            String name = sc.nextLine();// nextline stores can Names,Paragraphs,Sentences
+            System.out.println(name);
+
+            int number = sc.nextInt();//It takes numbers as Input
+            System.out.println(number);
+
+            float decimal = sc.nextFloat();//It takes float values as Input
+            System.out.println(decimal);
+
+            double price = sc.nextDouble();//It takes double values as Input
+            System.out.println(price);
+
+            long big= sc.nextLong();//It takes long values as Input
+            System.out.println(big);
+
+            boolean b = sc.nextBoolean();//It takes boolean values as Input
+            System.out.println(b);
+
+            short n= sc.nextShort();//It takes short values as Input
+            System.out.println(n);
+
 
     }
 }
