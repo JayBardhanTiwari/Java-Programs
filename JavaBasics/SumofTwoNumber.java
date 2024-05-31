9779dff376dc7b46760a8c15515746e21773f1ce
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-05-31 10:02:39 +0530

    SumofTwoNumber

---
+++ JavaBasics/SumofTwoNumber.java
@@ -0,0 +1,10 @@
+package JavaBasics;
+
+public class SumofTwoNumber {
+    public static void main(String[] args) {
+        int a = 10;
+        int b= 5;
+        int sum = a + b ;
+        System.out.println(sum);
+    }
+}
