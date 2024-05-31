e7abc14470fa3a0b811c7de292bcaf30e0f1c04b
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-05-31 10:34:40 +0530

    edited code in Datatypes

--- JavaBasics/Datatypes.java
+++ JavaBasics/Datatypes.java
@@ -2,16 +2,32 @@ package JavaBasics;
 
 public class Datatypes {
     public static void main(String args[]) {
-        byte b = 8; // Byte data type
-        short n = 240; // Short data type
-        char ch = 'a'; // Character data type
-        boolean var = true; // Boolean only store True or False
-        int a = 10; // Integer data type
-        long big = 1000; // Long data type
-        float f = 2.3; // Float data type
-        double tag = 10.5; // Double data type
 
-        // Total 8 data types in Primitive 
+        // In primitive Data types they are 8 data types
+
+        byte b = 8; // Byte data type
+        System.out.println(b);
+
+        short n = 240; // Short data type
+        System.out.println(n);
+
+        char ch = 'a'; // Character data type
+        System.out.println(ch);
+
+        boolean var = true; // Boolean only store True or False
+        System.out.println(var);
+
+        int a = 10; // Integer data type
+        System.out.println(a);
+
+        long big = 1000; // Long data type
+        System.out.println(big);
+
+        float decimal = 2.3f; // Float data type
+        System.out.println(decimal);
+        
+        double tag = 10.5; // Double data type
+        System.out.println(tag);
 
     }
 }
