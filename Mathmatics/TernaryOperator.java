6077c47e830984c85b5848d085b3354b69598f6e
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-01 12:25:09 +0530

    Ternary Operator

---
+++ Mathmatics/TernaryOperator.java
@@ -0,0 +1,10 @@
+package Mathmatics;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int marks = 55;
+        String reportCard = marks >= 33 ? "PASS": "FAIL";
+        System.out.println(reportCard);
+
+    }
+}
