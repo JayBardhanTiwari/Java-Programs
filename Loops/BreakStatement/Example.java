44166a65e35f658e74aee15cf31ec2688bfee996
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-02 10:56:34 +0530

    Break Statement

--- Loops/BreakStatement/Example.java
+++ Loops/BreakStatement/Example.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Example {
     public static void main(String[] args) {
         // Keep entering a number till user enters a multiple of 10
-
         Scanner sc = new Scanner(System.in);
         do {
             System.out.print("Enter your number : ");
