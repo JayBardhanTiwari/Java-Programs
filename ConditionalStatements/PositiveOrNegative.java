78f1c15133a1f0b77450e64581bf4465cdc86fb5
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-02 06:55:27 +0530

    Number is Positive Or Negative

---
+++ ConditionalStatements/PositiveOrNegative.java
@@ -0,0 +1,18 @@
+package ConditionalStatements;
+
+import java.util.Scanner;
+
+public class PositiveOrNegative {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if (number>0) {
+            System.out.println("Number is positive");
+            
+        }
+        else{
+            System.out.println("Number is negative");
+        }
+    }
+}
