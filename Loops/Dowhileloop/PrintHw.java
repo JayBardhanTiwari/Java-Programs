af02ab2d66ba085f8a5b975bf43b154a7fa0877d
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-02 10:23:27 +0530

    Do-while loop

---
+++ Loops/Dowhileloop/PrintHw.java
@@ -0,0 +1,12 @@
+package Loops.Dowhileloop;
+
+public class PrintHw {
+    public static void main(String[] args) {
+        int counter = 1;
+        do {
+            System.out.println("Hello world");
+            counter++;
+        } while (counter <= 10);
+    }
+
+}
