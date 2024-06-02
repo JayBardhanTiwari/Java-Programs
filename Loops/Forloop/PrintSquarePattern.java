d7ea83763a18fa1aa478c91c9c1f972aa17c9c27
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-02 09:38:20 +0530

    Print Square Pattern using for Loop

---
+++ Loops/Forloop/PrintSquarePattern.java
@@ -0,0 +1,9 @@
+package Loops.Forloop;
+
+public class PrintSquarePattern {
+    public static void main(String[] args) {
+        for (int line = 1; line <= 4; line++) {
+            System.out.println("****");
+        }
+    }
+}
