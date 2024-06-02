5ee9414dbe3c7c1ecec893f16ba7c8150c5428eb
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-02 07:01:16 +0530

    You have a FeverOrNot

---
+++ ConditionalStatements/FeverOrNot.java
@@ -0,0 +1,14 @@
+package ConditionalStatements;
+
+public class FeverOrNot {
+    public static void main(String[] args) {
+        double temp = 99.5;
+
+        if (temp > 100) {
+            System.out.println("You have a fever");
+
+        } else {
+            System.out.println("You dont have a fever");
+        }
+    }
+}
