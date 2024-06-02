22b6e2315e2d7fa0753e719c941d03ae7224795e
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-02 08:04:35 +0530

    Print Multiple times using While loop

---
+++ Loops/Printmultiple.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class Printmultiple {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 100) {
+            System.out.println("Hello World");
+            counter++;
+
+        }
+        System.out.println("Print Hello World 100x");
+    }
+}
