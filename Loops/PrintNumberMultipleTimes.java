b8ea6c95aac1fbedfedaedf56e945416cb3ba73d
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-02 08:15:55 +0530

    print number from 1 to 100

---
+++ Loops/PrintNumberMultipleTimes.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class PrintNumberMultipleTimes {
+    public static void main(String[] args) {
+        // Using while loops
+        int counter = 1;
+        while (counter <= 100) {
+            System.out.print(counter + " ");
+            counter++;
+
+        }
+    }
+}
