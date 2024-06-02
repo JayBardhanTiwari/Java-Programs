ab0af489ed33963f21c7b51af7b9fc01ca39a723
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-02 09:12:50 +0530

    Optimized and using for loop

--- OperatorsInJava/SumOfNaturalNumbers.java
+++ OperatorsInJava/SumOfNaturalNumbers.java
@@ -2,18 +2,15 @@ package OperatorsInJava;
 
 public class SumOfNaturalNumbers {
     public static void main(String[] args) {
-        
-        int num= 4;
-        int sum = 0;
-        int add = 0;
-        for(int i=0;i<num;i++){
 
-            sum = sum + 1;
-            add = sum + add;
-           
+        int num = 5;
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+
+            sum += i;
 
         }
-        System.out.println(add);
+        System.out.println(sum);
     }
-    
+
 }
