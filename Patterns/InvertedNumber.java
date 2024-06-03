440f2156cecf54acfd25210eec67953431210356
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-03 09:35:39 +0530

    Inverted Number

---
+++ Patterns/InvertedNumber.java
@@ -0,0 +1,16 @@
+package Patterns;
+
+public class InvertedNumber {
+    public static void main(String[] args) {
+        int n =5;
+        
+        
+        for(int i =1;i<=n;i++){
+            for(int number = 1;number<=n-i+1;number++){
+                System.out.print(number);
+                
+            }
+            System.out.println();
+        }
+    }
+}
