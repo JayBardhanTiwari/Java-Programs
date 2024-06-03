c355e9e0facab8f2ef9ff5e4b781c116de48e833
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-03 08:12:22 +0530

    Inverted Star Pattern

---
+++ Patterns/InvertedStarPattern.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+public class InvertedStarPattern {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int line = 1; line <= n; line++) {
+            for (int star = 1; star <= (n - line + 1); star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+ * Another way to print inverted star pattern
+ * for(int line = 4;line<=4;line--){
+ * for(int star=1;star<=line;star++){
+ * System.out.print("*");
+ * }
+ * System.out.println();
+ * }
+ */
