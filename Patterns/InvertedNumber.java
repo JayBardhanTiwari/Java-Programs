982cd71269a485af6bc97a19becd6bf874c95365
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-03 09:36:19 +0530

    Inverted Number clean code

--- Patterns/InvertedNumber.java
+++ Patterns/InvertedNumber.java
@@ -2,13 +2,12 @@ package Patterns;
 
 public class InvertedNumber {
     public static void main(String[] args) {
-        int n =5;
-        
-        
-        for(int i =1;i<=n;i++){
-            for(int number = 1;number<=n-i+1;number++){
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int number = 1; number <= n - i + 1; number++) {
                 System.out.print(number);
-                
+
             }
             System.out.println();
         }
