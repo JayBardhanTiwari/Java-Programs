1fe7fe24ddd973d968288a211068d8be0a3f6af7
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-03 07:48:42 +0530

    Print Star

---
+++ Patterns/PrintStar.java
@@ -0,0 +1,14 @@
+package Patterns;
+
+public class PrintStar {
+    public static void main(String[] args) {
+        for (int line = 1; line <= 4; line++) {
+            //for lines
+            for (int star = 1; star <= line; star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
