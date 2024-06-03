206008016ab2da13156ac04de67d72143fce636b
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-03 11:28:52 +0530

    Parameters and Arguments

--- Functions_Methods/FuncWithParameters.java
+++ Functions_Methods/FuncWithParameters.java
@@ -3,7 +3,8 @@ package Functions_Methods;
 import java.util.Scanner;
 
 public class FuncWithParameters {
-    public static int CalculateSum(int num1, int num2) {
+    public static int CalculateSum(int num1, int num2) //Formal parmeter or parameter
+    {
 
         int sum = num1 + num2;
         return sum;
@@ -13,7 +14,7 @@ public class FuncWithParameters {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int sum = CalculateSum(a, b);
+        int sum = CalculateSum(a, b);// Actual parameters or arguments
         System.out.println("sum is : " +sum);
 
     }
