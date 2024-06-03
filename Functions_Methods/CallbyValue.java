c88db7e12293b21e3d87d2a69e388926a9eb6427
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-03 12:04:14 +0530

    Definition of CallbyValue

--- Functions_Methods/CallbyValue.java
+++ Functions_Methods/CallbyValue.java
@@ -3,22 +3,22 @@ package Functions_Methods;
 public class CallbyValue {
 
     public static void Swap(int a, int b) {
-    //swap
-    int temp= a;
-        a=b;
-        b= temp;
+        // swap
+        int temp = a;
+        a = b;
+        b = temp;
 
     }
+
     public static void main(String[] args) {
-        
         int a = 5;
         int b = 10;
-      
         Swap(a, b);
         System.out.println("Value os a : " + a);
         System.out.println("Value of b : " + b);
-   
-  
     }
-
 }
+/*
+ * When you pass a value in function it does not pass the original value of that variable
+ * it makes a copy of original value then that copy goes to the function
+ */
