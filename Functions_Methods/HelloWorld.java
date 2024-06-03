6630364382e965131e5e5ce43ccbb1ef887596fa
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-03 10:17:03 +0530

    Functions_Methods

---
+++ Functions_Methods/HelloWorld.java
@@ -0,0 +1,18 @@
+package Functions_Methods;
+
+public class HelloWorld {
+    // creating a function
+    //In java function is created in classes so conventionally its a method
+    //Means those function which is created in classes known as Methods
+    public static void PrintHelloWorld() {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+    }
+
+    public static void main(String[] args) {
+        PrintHelloWorld();// calling a funtion
+
+    }
+}
