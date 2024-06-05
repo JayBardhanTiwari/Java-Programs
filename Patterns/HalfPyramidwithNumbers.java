3cd6868c43b4f2d0993340d27a6145e8cc4e614e
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-05 11:57:48 +0530

    Half Pyramid with Numbers

--- Patterns/HalfPyramidwithNumbers.java
+++ Patterns/HalfPyramidwithNumbers.java
@@ -1,6 +1,6 @@
 package Patterns;
 
-public class HalfPyramid {
+public class HalfPyramidwithNumbers {
     public static void main(String[] args) {
         int n = 10;
         for (int line = 1; line <= n; line++) {
