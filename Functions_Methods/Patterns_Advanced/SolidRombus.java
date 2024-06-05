47a7b3bdeecd0ce9a0824c050c9baa6374e92563
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-05 14:10:08 +0530

    Solid Rombus

---
+++ Functions_Methods/Patterns_Advanced/SolidRombus.java
@@ -0,0 +1,23 @@
+package Functions_Methods.Patterns_Advanced;
+
+public class SolidRombus {
+    public static void Rombas(int n) {
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= (n - i); j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Rombas(6);
+    }
+}
