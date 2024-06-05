7c44c817f06aa4dacd5ded7ff3bec6de12b8b8df
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-05 12:46:29 +0530

    Zero and One Triangle

---
+++ Functions_Methods/Patterns_Advanced/ZeroandOneTriangle.java
@@ -0,0 +1,21 @@
+package Functions_Methods.Patterns_Advanced;
+
+public class ZeroandOneTriangle {
+    public static void Zero_One_Triangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Zero_One_Triangle(5);
+
+    }
+}
