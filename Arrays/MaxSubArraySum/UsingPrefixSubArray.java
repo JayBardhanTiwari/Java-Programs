d4af011494dd83e6ee3c469b4e0111f2b92a1eb3
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-08 10:15:44 +0530

    Using Prefix Sum approach(Max Sub Array Sum)

---
+++ Arrays/MaxSubArraySum/UsingPrefixSubArray.java
@@ -0,0 +1,37 @@
+package MaxSubArraySum;
+
+public class UsingPrefixSubArray {
+
+    // Time complexity = O(n²)
+
+    public static void prefixSubarray(int number[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[number.length];
+
+        // calculate prefix array
+        prefix[0] = number[0];
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + number[i];
+
+        }
+
+        for (int i = 0; i < number.length; i++) {
+            int start = i;
+            for (int j = i; j < number.length; j++) {
+                int end = j;
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("Max Sum = : " + maxSum);
+    }
+
+    public static void main(String[] args) {
+        int number[] = { 1, -2, 6, -1, 3 };
+        prefixSubarray(number);
+    }
+}
