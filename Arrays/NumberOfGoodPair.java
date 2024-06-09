fe44c948cf006b00cfb02160d2276c4c0475f751
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-09 11:13:31 +0530

    Number Of Good Pair

---
+++ Arrays/NumberOfGoodPair.java
@@ -0,0 +1,23 @@
+public class NumberOfGoodPair {
+        public static int numIdenticalPairs(int[] nums) {
+                  int tp = 0;
+    
+            for (int i = 0; i < nums.length; i++) {
+                int count =nums[i];
+    
+                for (int j = i + 1; j < nums.length; j++) {
+                    if(count==nums[j]){
+                        tp++;
+                    }
+    
+                }
+            }
+            return tp;
+        }
+    
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,1,1,3};
+        System.out.println(numIdenticalPairs(nums));
+        
+    }
+}
