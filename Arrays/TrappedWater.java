3ca9487d45dec32bfb08a0c59207dbd1ccde4a4f
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-11 00:38:00 +0530

    Trapped Water code (Medium question of Arrays)

---
+++ Arrays/TrappedWater.java
@@ -0,0 +1,35 @@
+public class TrappedWater {
+
+    public static int trappedWater(int height[]) {
+        int n = height.length;
+        // to find left max boundary
+        int leftmax[] = new int[n];
+        leftmax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftmax[i] = Math.max(height[i], leftmax[i - 1]);
+        }
+        // to find right max boundary
+        int rightmax[] = new int[n];
+        rightmax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightmax[i] = Math.max(height[i], rightmax[i + 1]);
+
+        }
+
+        int trappedWater = 0;
+        for (int i = 0; i < n; i++) {
+            // waterlevel = min(leftmax,rightmax)
+            int waterlevel = Math.min(leftmax[i], rightmax[i]);
+            // trappedWater = waterlevel - height[i]
+            trappedWater += waterlevel - height[i];
+        }
+        return trappedWater;
+
+    }
+
+    public static void main(String[] args) {
+        int height[] = { 4, 2, 0, 6, 3, 2, 5 };
+        System.out.println(trappedWater(height));
+
+    }
+}
