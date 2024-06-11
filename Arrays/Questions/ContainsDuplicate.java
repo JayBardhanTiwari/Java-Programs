d0c287faa6edb930390e4a1e13b0e7c35df7097f
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-11 18:40:02 +0530

    edited

--- Arrays/Questions/ContainsDuplicate.java
+++ Arrays/Questions/ContainsDuplicate.java
@@ -1,11 +1,9 @@
 package Questions;
 
-import java.util.Arrays;
-
 public class ContainsDuplicate {
     public static boolean duplicateArrays(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j< nums.length; j++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] == nums[j]) {
                     return true;
                 }
