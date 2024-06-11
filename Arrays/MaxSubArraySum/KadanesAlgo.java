2c611757441894fd04401cbb3903a22849b242fc
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-11 17:10:58 +0530

    Kadanes Algo

--- Arrays/MaxSubArraySum/KadanesAlgo.java
+++ Arrays/MaxSubArraySum/KadanesAlgo.java
@@ -1,23 +1,26 @@
 package MaxSubArraySum;
 
 public class KadanesAlgo {
-    public static void Kadanes(int number[]) {
+    public static int Kadanes(int number[]) {
         int maxSum = Integer.MIN_VALUE;
         int currSum = 0;
         for (int i = 0; i < number.length; i++) {
-            currSum = currSum + number[i];
+            currSum += number[i];
+            if (currSum > maxSum) {
+                maxSum = currSum;
+            }
             if (currSum < 0) {
                 currSum = 0;
             }
-            maxSum = Math.max(maxSum, currSum);
 
         }
-        System.out.println("Max sum is : " + maxSum);
+        return maxSum;
+
     }
 
     public static void main(String[] args) {
-        int number[] = { -2, -3, 4, -1, -2, 1, 5, -3 };
-        Kadanes(number);
+        int number[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        System.out.println(Kadanes(number));
 
     }
 
