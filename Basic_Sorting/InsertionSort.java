22acb24c3fd639533ff79bdd376b3592df2c42b6
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-13 01:36:38 +0530

    Insertion Sort

---
+++ Basic_Sorting/InsertionSort.java
@@ -0,0 +1,30 @@
+package Basic_Sorting;
+
+public class InsertionSort {
+    public static void insertionSort(int nums[]) {
+        for (int i = 1; i < nums.length; i++) {
+            int curr = nums[i];
+            int prev = i - 1;
+            // finding out the correct position to insert
+            while (prev >= 0 && nums[prev] > curr) {
+                nums[prev + 1] = nums[prev];
+                prev--;
+            }
+            // insertion
+            nums[prev + 1] = curr;
+        }
+
+    }
+
+    public static void printArray(int nums[]) {
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 5, 4, 1, 3, 2 };
+        insertionSort(nums);
+        printArray(nums);
+    }
+}
