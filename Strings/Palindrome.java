37f972faf02ac82253016e3d76e3ef791d610842
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-20 00:35:24 +0530

    Check a String  is Palindrome or not

---
+++ Strings/Palindrome.java
@@ -0,0 +1,20 @@
+package Strings;
+
+class Palindrome {
+
+    public static boolean isPalindrome(String str) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            int n = str.length();
+            if (str.charAt(i) != str.charAt(n - i - 1)) {
+                // if its not a palindrome
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "racecar";
+        System.out.println(isPalindrome(str));
+    }
+}
