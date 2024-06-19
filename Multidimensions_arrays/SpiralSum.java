32675f081187299b6af3fb1cf82479e1f8af4410
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-19 20:04:48 +0530

    Spiral Sum (Question)

---
+++ Multidimensions_arrays/SpiralSum.java
@@ -0,0 +1,39 @@
+package Multidimensions_arrays;
+
+public class SpiralSum {
+    public static int spiralSum(int matrix[][]) { // time complexity O(n^2)
+        // primary diagonal
+        int sum = 0;
+
+        // for (int i = 0; i < matrix.length; i++) {
+        // for (int j = 0; j < matrix[0].length; j++) {
+        // if (i == j) {
+        // sum += matrix[i][j];
+        // //secondary diagonal
+        // } else if (i + j == matrix.length - 1) {
+        // sum += matrix[i][j];
+        // }
+        //
+        // }
+        // optimized code
+        for (int i = 0; i < matrix.length; i++) {// time complexity O(n)
+            sum += matrix[i][i];
+            if (i != matrix.length - 1 - i) {
+                sum += matrix[i][matrix.length - 1 - i];
+
+            }
+
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = { { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 } };
+        System.out.println(spiralSum(matrix));
+
+    }
+}
