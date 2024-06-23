718fa5c779092c0b025c434e9d6bdeb79b390fee
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-23 20:58:20 +0530

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,7 +1,6 @@
 public class StringExample{    
 public static void main(String args[]){    
 String s1="java";//creating string by Java string literal    
-char ch[]={'s','t','r','i','n','g','s'};    
 String s2=new String(ch);//converting char array to string    
 System.out.println(s1);    
 System.out.println(s2);     
