dd35479983383f9ead6207a27e6c92bee8f49bbb
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-27 21:33:24 +0530

    String Comparison

---
+++ Strings/stringComparison.java
@@ -0,0 +1,25 @@
+package Strings;
+public class stringComparison {
+    public static void main(String[] args) {
+        
+        String str1 = "tony";
+        String str2 = "tony";
+        String str3 = new String("tony");
+
+        if(str1 == str2){
+            System.out.println("strings are euqals");
+
+        }
+        else{
+            System.out.println("not equal");
+        }
+        if(str1 == str3){
+            System.out.println("strings are euqals");
+
+        }
+        else{
+            System.out.println("not equal");
+        }
+    }
+
+}
