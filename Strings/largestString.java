8eaa7e2ccc363f46c9bed59a671b36716b1c3b3a
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-06-29 17:38:59 +0530

    largestString

---
+++ Strings/largestString.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class largestString {
+
+    public static void main(String[] args) {
+        String fruits[] = { "apple", "mango", "banana" };
+        // here we are comparing these strings using lexicographic order
+
+        String largest = fruits[0];
+        for (int i = 1; i < fruits.length; i++) {
+            if (largest.compareToIgnoreCase(fruits[i]) < 0) {
+                largest = fruits[i];
+            }
+        }
+        System.out.println(largest);
+
+    }
+}
