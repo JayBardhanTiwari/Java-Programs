de437cd3e691e244bb7d22b162a0e18aa3b755d7
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-07-01 22:04:12 +0530

    leetcode

--- Example.java
+++ Example.java
@@ -1,5 +1,21 @@
-public class StringExample{    
-public static void main(String args[]){    
-String s1="java";//creating string by Java string literal    
-String s2=new String(ch);//converting char array 
-}}    
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static int asciiValue(String sc){
+        int asciivalue = 0;
+        int total = 0;
+        for(int i = 0 ; i < sc.length()-1;i++){
+            asciivalue = (int)sc.charAt(i) - (int)sc.charAt(i+1);        
+             total += Math.abs(asciivalue);
+            
+
+        }
+        return total;
+    }
+    public static void main(String[] args) {
+        String sc = "zaz";
+        System.out.println(asciiValue(sc));
+        
+    }
+}
