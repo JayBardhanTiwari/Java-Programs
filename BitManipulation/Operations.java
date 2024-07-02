3bc732d9887d6d31d89f887927b4d8964c59b35c
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-07-02 22:11:31 +0530

    clear ith bit

--- BitManipulation/Operations.java
+++ BitManipulation/Operations.java
@@ -18,8 +18,15 @@ public class Operations {
         return n | bitmask;
     }
 
+    // 3.clear ith bit
+    public static int clearIthbit(int n, int i) {
+        int bitmask = ~(1 << i);
+        return n & bitmask;
+    }
+
     public static void main(String[] args) {
         System.out.println(getIthbit(10, 3));
         System.out.println(setIthbit(10, 2));
+        System.out.println(clearIthbit(10, 1));
     }
 }
