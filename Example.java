d4cf82dfef959a48c587da9200cfed11abbd48e5
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-07-04 20:52:28 +0530

    edited

--- Example.java
+++ Example.java
@@ -1,21 +1,19 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
 
-class HelloWorld {
-    public static int asciiValue(String sc){
+class Example {
+    public static int asciiValue(String sc) {
         int asciivalue = 0;
         int total = 0;
-        for(int i = 0 ; i < sc.length()-1;i++){
-            asciivalue = (int)sc.charAt(i) - (int)sc.charAt(i+1);        
-             total += Math.abs(asciivalue);
-            
+        for (int i = 0; i < sc.length() - 1; i++) {
+            asciivalue = (int) sc.charAt(i) - (int) sc.charAt(i + 1);
+            total += Math.abs(asciivalue);
 
         }
         return total;
     }
+
     public static void main(String[] args) {
         String sc = "zaz";
         System.out.println(asciiValue(sc));
-        
+
     }
 }
