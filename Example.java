6b1ca5647221e60fa254ccb95b4abed65ff376b5
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-07-05 11:05:54 +0530

    edited

--- Example.java
+++ Example.java
@@ -1,19 +1,15 @@
-
-class Example {
-    public static int asciiValue(String sc) {
-        int asciivalue = 0;
-        int total = 0;
-        for (int i = 0; i < sc.length() - 1; i++) {
-            asciivalue = (int) sc.charAt(i) - (int) sc.charAt(i + 1);
-            total += Math.abs(asciivalue);
-
+public class Example {
+    public static void minimumOperations(int nums[]){
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] %3 == 0){
+                count++;
+            }
+            else 
         }
-        return total;
     }
 
     public static void main(String[] args) {
-        String sc = "zaz";
-        System.out.println(asciiValue(sc));
-
+        int nums [] = { 1,2,3,4};
     }
 }
