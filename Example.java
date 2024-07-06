5b1e28877128d48c47da211f51fafb70b7ea639e
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-07-06 23:50:07 +0530

    edited

--- Example.java
+++ Example.java
@@ -1,15 +1,31 @@
+
 public class Example {
-    public static void minimumOperations(int nums[]){
-        int count = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] %3 == 0){
-                count++;
-            }
-            else 
+    // 1. Get ith bit 
+    public static int getIthbit(int n, int i) {
+        int bitmask = 1 << i;
+        if ((n & bitmask) == 0) {
+            return 0;
+        } else {
+            return 1;
         }
+
+    }
+
+    // 2. set ith bit
+    public static int setIthbit(int n, int i) {
+        int bitmask = 1 << i;
+        return n | bitmask;
+    }
+
+    // 3.clear ith bit
+    public static int clearIthbit(int n, int i) {
+        int bitmask = ~(1 << i);
+        return n & bitmask;
     }
 
     public static void main(String[] args) {
-        int nums [] = { 1,2,3,4};
+        // System.out.println(getIthbit(6, 2 ));
+        // System.out.println(setIthbit(13, 2));
+        System.out.println(clearIthbit(11 ,, 1));
     }
 }
