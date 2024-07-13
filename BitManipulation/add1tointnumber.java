0120e6085b9e4444e08956cafe97d94066989bf0
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-07-13 12:52:59 +0530

    Add 1 to an integer using Bit Manipulation

---
+++ BitManipulation/add1tointnumber.java
@@ -0,0 +1,18 @@
+package BitManipulation;
+
+public class add1tointnumber {
+    public static void main(String[] args) {
+
+        // using Bitwise NOT Operator
+
+        int x = 6;
+        System.out.println(x + " + " + 1 + " is " + -~x);
+
+        x = -4;
+        System.out.println(x + " + " + 1 + " is " + -~x);
+
+        x = 0;
+        System.out.println(x + " + " + 1 + " is " + -~x);
+
+    }
+}
