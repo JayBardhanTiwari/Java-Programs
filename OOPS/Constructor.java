0efd18d021cedf48e6ede8130f87cce9ffef5aa2
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-07-16 08:17:01 +0530

    Constructor

---
+++ OOPS/Constructor.java
@@ -0,0 +1,32 @@
+package OOPS;
+
+public class Constructor {
+    public static void main(String[] args) {
+        Student s1 = new Student("Jay Bardhan");
+        System.out.println(s1.name);
+        Car c1 = new Car();
+
+    }
+}
+
+class Student {
+    String name;
+    int age;
+
+    Student(String name) {
+        this.name = name;
+
+    }
+
+}
+
+// if you dont want to pass the value in constructor then
+
+class Car {
+    String name;
+    int modelnumber;
+
+    Car() {
+        System.out.println("Constructor is called....");
+    }
+}
