a1dd3b01ed5dfb4237e511748e917a2ebdf56a35
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-07-18 12:11:40 +0530

    Factorial of a Number

---
+++ RecursionBasics/Factorialof_n.java
@@ -0,0 +1,21 @@
+package RecursionBasics;
+
+public class Factorialof_n {
+
+    public static int findFac(int n) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        int Fnm = findFac(n - 1);
+        int Fn = n * findFac(n - 1);
+
+        return Fn;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findFac(5));
+
+    }
+}
