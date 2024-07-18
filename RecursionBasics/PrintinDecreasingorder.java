656c1c99451bcb1d3c3b99f829b48fad718a201e
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-07-18 10:11:41 +0530

    Recursion Basics -> Print in Decreasing order

---
+++ RecursionBasics/PrintinDecreasingorder.java
@@ -0,0 +1,18 @@
+package RecursionBasics;
+
+public class PrintinDecreasingorder {
+
+    public static void printDec(int n) {
+        if (n == 1) {       //stops when n = 1 
+            System.out.println(1);
+            return;
+        }
+        System.out.print(n + " ");
+        printDec(n - 1);// calling itself
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        printDec(n);
+    }
+}
