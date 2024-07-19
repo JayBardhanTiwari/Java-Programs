bedb794ce25ee26c99ce5feab13ea267a7bb5907
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-07-19 13:04:32 +0530

    edited

--- RecursionBasics/Findfirstoccurence_inArray.java
+++ RecursionBasics/Findfirstoccurence_inArray.java
@@ -2,7 +2,7 @@ package RecursionBasics;
 
 public class Findfirstoccurence_inArray {
 
-    public static int findOccurence(int array[], int key, int i) {
+    public static int firstOccurence(int array[], int key, int i) {
         if (i == array.length) {
             return -1;
         }
@@ -11,12 +11,12 @@ public class Findfirstoccurence_inArray {
             return i;
         }
 
-        return findOccurence(array, key, i + 1);
+        return firstOccurence(array, key, i + 1);
     }
 
     public static void main(String[] args) {
         int array[] = { 1, 2, 4, 5, 7 };
-        System.out.println(findOccurence(array, 7, 0));
+        System.out.println(firstOccurence(array, 7, 0));
     }
 
 }
