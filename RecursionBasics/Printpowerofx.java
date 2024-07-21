766516b59e3a67b5557a5482ef817946f8246984
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-07-21 15:15:14 +0530

    edited

--- RecursionBasics/Printpowerofx.java
+++ RecursionBasics/Printpowerofx.java
@@ -1,16 +1,16 @@
 package RecursionBasics;
 
 public class Printpowerofx {
-    public static int printPower(int x,int n){
-        if(n == 1){
-            return x;
+    public static int printPower(int x, int n) {
+        if (n == 0) {
+            return 1;
         }
-        int nm1 = printPower(x, n-1);
+        int nm1 = printPower(x, n - 1);
         int total = x * nm1;
         return total;
     }
+
     public static void main(String[] args) {
-        int x = 2;
-        System.out.println(printPower(x, 10));
+        System.out.println(printPower(2, 10));
     }
 }
