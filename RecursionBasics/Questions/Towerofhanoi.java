084fc6fe10be04bdfc3d9fa0071455c1cbe7bbda
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-07-23 16:35:20 +0530

    Tower of Hanoi using recursion

---
+++ RecursionBasics/Questions/Towerofhanoi.java
@@ -0,0 +1,23 @@
+package RecursionBasics.Questions;
+
+public class Towerofhanoi {// 0(2^n)
+    public static void moveDisks(int n, String src, String helper, String des) {
+
+        // base case
+        if (n == 1) {
+            System.out.println("Transfer disk " + n + " from " + src + " to " + des);
+            return;
+
+        }
+        moveDisks(n - 1, src, des, helper);
+        System.out.println("Transfer disk " + n + " from " + src + " to" + des);
+        moveDisks(n - 1, helper, src, des);
+
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        moveDisks(n, "A", "B", "C");
+
+    }
+}
