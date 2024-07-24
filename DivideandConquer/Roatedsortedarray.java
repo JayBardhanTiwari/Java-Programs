108238612ba166fd273281b4b72bdfa1a9f5da26
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-07-24 19:34:28 +0530

    edited

--- DivideandConquer/Roatedsortedarray.java
+++ DivideandConquer/Roatedsortedarray.java
@@ -2,7 +2,7 @@ package DivideandConquer;
 
 class Roatedsortedarray {
 
-    public static int search(int arr[], int key, int si, int ei) {
+    public static int search(int nums[], int key, int si, int ei) {
         // base case
         if (si > ei) {
             return -1;
@@ -10,38 +10,38 @@ class Roatedsortedarray {
         int mid = si + (ei - si) / 2;// (si+ ei )/ 2
 
         // case Found
-        if (arr[mid] == key) {
+        if (nums[mid] == key) {
             return mid;
         }
 
         // mid on line 1
-        if (arr[si] <= arr[mid]) {
+        if (nums[si] <= nums[mid]) {
             // case a: left
-            if (arr[si] <= key && key <= arr[mid]) {
-                return search(arr, key, si, mid - 1);
+            if (nums[si] <= key && key <= nums[mid]) {
+                return search(nums, key, si, mid - 1);
             }
             // case b: right
             else {
-                return search(arr, key, mid + 1, ei);
+                return search(nums, key, mid + 1, ei);
             }
         }
         // mid on line 2
 
         else {
             // case c: right
-            if (arr[mid] <= key && key <= arr[ei]) {
-                return search(arr, key, mid + 1, ei);
+            if (nums[mid] <= key && key <= nums[ei]) {
+                return search(nums, key, mid + 1, ei);
 
             } else {
                 // case d : left
-                return search(arr, key, si, mid - 1);
+                return search(nums, key, si, mid - 1);
             }
         }
     }
 
     public static void main(String[] args) {
-        int arr[] = { 4, 5, 6, 7, 0, 1, 2 };
+        int nums[] = { 4, 5, 6, 7, 0, 1, 2 };
         int key = 0;// output -> 4
-        System.out.println(search(arr, key, 0, arr.length - 1));
+        System.out.println(search(nums, key, 0, nums.length - 1));
     }
 }
