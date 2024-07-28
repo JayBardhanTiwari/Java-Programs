dd226442539e550d3d221cb7341eac18d67013ac
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-07-28 20:31:29 +0530

    edited

--- Example2.java
+++ Example2.java
@@ -1,5 +1,6 @@
 public class Example2 {
 
+    // array
     public static void getArray(int arr[], int i, int val) {
         // base case
         if (i == arr.length) {
@@ -9,65 +10,39 @@ public class Example2 {
         // recursion
         arr[i] = val;
         getArray(arr, i + 1, val + 1);
-        // backtracking
-        arr[i] = val - 2;
+        arr[i] = arr[i] - 2;
 
     }
 
-    // array
     public static void printArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
-
     // subsets of string
-    public static void getSubset(String str, String ans, int i) {
-        // base case
-        if (i == str.length()) {
-            if (ans == "") {
-                System.out.println("null");
-            } else {
-                System.out.println(ans);
-
-            }
-            return;
-        }
-        // recursion
-        getSubset(str, ans + str.charAt(i), i + 1);
-        // no
-        getSubset(str, ans, i + 1);
-
-    }
-
-    // permutation
-    public static void permutation(String str, String ans) {
-        // base case
-        if (str.length() == 0) {
+    public static void subString(String str , String ans,int i ){
+        //base case 
+        if(str.length() == i){
             System.out.println(ans);
             return;
         }
-        // recursion
-        for (int i = 0; i < str.length(); i++) {
-            // "abcdef" = "ab" + "def" = "abdef"
-            // fetch
-            char curr = str.charAt(i);
-            // remove
-            String newStr = str.substring(0, i) + str.substring(i + 1);
-            // add
-            permutation(newStr, ans + curr);
-        }
 
+        // recursion 
+        //yes 
+       subString(str, ans+str.charAt(i),i+1);
+       subString(str, ans,i+1);
     }
+
+    // permutation
 
     public static void main(String[] args) {
 
         // int arr[] = new int[5];
         // getArray(arr, 0, 1);
         // printArr(arr);
-        // getSubset("abc", "", 0);
-        permutation("abc", "");
+        String str = "abc";
+        subString(str, "",0);
 
     }
 }
---
+++ Test.java
@@ -0,0 +1,76 @@
+public class Test {
+
+    public static boolean isSafe(char board[][], int row, int column) {
+        // vertically up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][column] == 'Q') {
+                return false;
+            }
+
+        }
+
+        // diagonal left
+        for (int i = row - 1, j = column - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // diagonal right
+        for (int i = row - 1, j = column + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+
+            }
+        }
+        return true;
+    }
+
+    public static void nQueen(char board[][], int row) {
+        // base case
+        if (row == board.length) {
+            printBoard(board);
+            
+            return;
+        }
+        // loop for column
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q'; // placing queen
+                nQueen(board, row + 1); // function call
+                board[row][j] = 'x'; // backtracking call
+            }
+
+        }
+
+    }
+
+    // function for print board
+    public static void printBoard(char board[][]) {
+        System.out.println("------chess board-------");
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static int count = 0;
+
+    public static void main(String[] args) {
+        int n = 4;
+        char board[][] = new char[n][n];
+        // intialization
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'x';
+            }
+        }
+        nQueen(board, 0);
+        System.out.println("total ways to solve n queens = " + count);
+
+    }
+
+}
