ca35981b50a0bacb40a8373a8d152dfe05c7ad7f
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-07-30 20:10:24 +0530

    Multidimensional Array List

---
+++ ArrayList/Multidimensionalarraylist.java
@@ -0,0 +1,38 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Multidimensionalarraylist {
+    public static void main(String[] args) {
+
+        ArrayList<ArrayList<Integer>> mainList = new ArrayList<>();
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        ArrayList<Integer> list3 = new ArrayList<>();
+
+        // value intialization
+        for (int i = 1; i <= 5; i++) {
+            list1.add(i * 1);// 1 2 3 4 5
+            list2.add(i * 2);// 2 4 6 8 10
+            list3.add(i * 3);// 3 6 9 12 15
+        }
+        mainList.add(list1);
+        mainList.add(list2);
+        mainList.add(list3);
+        System.out.println(mainList);
+
+        // nested loop for print multidimension Array list
+        for (int i = 0; i < mainList.size(); i++) {
+            // System.out.println(mainList.get(i));
+            ArrayList<Integer> currList = mainList.get(i);
+
+            for (int j = 0; j < currList.size(); j++) {
+                System.out.print(currList.get(j) + " ");
+
+            }
+            System.out.println();
+        }
+    }
+
+}
