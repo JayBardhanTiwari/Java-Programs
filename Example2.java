bba272fd1ff96c5c67c608b9c5b4777c164610a9
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-07-31 20:14:50 +0530

    Revision of backtracking n Array list

--- Example2.java
+++ Example2.java
@@ -1,7 +1,8 @@
+
 public class Example2 {
 
     // array
-    public static void getArray(int arr[], int i, int val) {
+    public static void backtrackArray(int arr[], int i, int val) {
         // base case
         if (i == arr.length) {
             printArr(arr);
@@ -9,9 +10,8 @@ public class Example2 {
         }
         // recursion
         arr[i] = val;
-        getArray(arr, i + 1, val + 1);
-        arr[i] = arr[i] - 2;
-
+        backtrackArray(arr, i + 1, val + 1);
+        arr[i] = val - 2;
     }
 
     public static void printArr(int arr[]) {
@@ -20,29 +20,158 @@ public class Example2 {
         }
         System.out.println();
     }
+
     // subsets of string
-    public static void subString(String str , String ans,int i ){
-        //base case 
-        if(str.length() == i){
+    public static void subsetString(String str, String ans, int i) {
+        // base case
+        if (i == str.length()) {
+
+            if (ans == "") {
+                System.out.println("null");
+
+            } else {
+                System.out.println(ans);
+
+            }
+            return;
+        }
+
+        // recursion
+        subsetString(str, ans + str.charAt(i), i + 1);
+        subsetString(str, ans, i + 1);
+
+    }
+
+    // permutation
+    public static void permutationString(String str, String ans) {
+        // base case
+        if (str.length() == 0) {
             System.out.println(ans);
             return;
         }
 
-        // recursion 
-        //yes 
-       subString(str, ans+str.charAt(i),i+1);
-       subString(str, ans,i+1);
+        // recursion
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            // remove
+            String newStr = str.substring(0, i) + str.substring(i + 1);
+            permutationString(newStr, ans + ch);
+
+        }
     }
 
-    // permutation
+    // issafe function
+    public static boolean isSafe(char board[][], int row, int column) {
+        // vertical up
+        // isSafe(board, row-1, column);
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][column] == 'Q') {
+                return false;
+            }
+        }
+
+        // diagonal left
+        for (int i = row - 1, j = column - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // diagonal right
+        for (int i = row - 1, j = column + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // N-queens
+    public static boolean nQueens(char board[][], int row) {
+        // base case
+        if (row == board.length) {
+            // printBoard(board);
+            count++;
+            return true;
+        }
+        // loop for columns
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                if (nQueens(board, row + 1)) {
+                    return true;
+                } else {
+                    board[row][j] = 'x';
+                }
+
+            }
+
+        }
+        return false;
+
+    }
+
+    // print board
+    public static void printBoard(char board[][]) {
+        System.out.println("------chess board------");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static int count = 0;
+
+    // GRID WAYS
+    public static int gridWays(int n, int m, int i, int j) {
+        // base case
+        if (i == n - 1 && j == m - 1) {
+            return 1;
+        } else if (i == n || j == m) {
+            return 0;
+        }
+
+        // recursion
+        int right = gridWays(n, m, i, j + 1);
+        int down = gridWays(n, m, i + 1, j);
+
+        return right + down;
+    }
 
     public static void main(String[] args) {
-
         // int arr[] = new int[5];
-        // getArray(arr, 0, 1);
+        // backtrackArray(arr, 0, 1);
         // printArr(arr);
-        String str = "abc";
-        subString(str, "",0);
+        // subsetString("abc", "", 0);
+        // permutationString("abc", " ");
+
+        // N- QUEENS QUESTION
+        // int n = 4;
+
+        // char board[][] = new char[n][n];
+
+        // // intialization
+        // for (int i = 0; i < n; i++) {
+        // for (int j = 0; j < n; j++) {
+        // board[i][j] = 'x';
+        // }
+        // }
+        // nQueens(board, 0);
+        // // System.out.println(count);
+        // // printBoard(board);//
+        // if (nQueens(board, n)) {
+        // System.out.println("solution is possible");
+        // printBoard(board);
+        // } else {
+        // System.out.println("solution is not possible ");
+        // }
+
+        // GRID WAYS
+        int n = 3;// rows
+        int m = 3;// columns
+        System.out.println(gridWays(n, m, 0, 0));
 
     }
 }
---
+++ Example4.java
@@ -0,0 +1,154 @@
+import java.util.*;
+
+public class Example4 {
+    public static void swap(ArrayList<Integer> list2, int idx1, int idx2) {
+        // swaping
+        int temp = list2.get(idx1);
+        list2.set(idx1, list2.get(idx2));
+        list2.set(idx2, temp);
+
+    }
+
+    // public static int calWater(ArrayList<Integer> height){
+
+    // brute force
+
+    // int maxWater = 0;
+
+    // for(int i =0;i<height.size();i++){
+    // for(int j=i+1;j<height.size();j++){
+    // int ht = Math.min(height.get(i),height.get(j));
+    // int width = j-i;
+
+    // int currWater = ht * width;
+
+    // maxWater = Math.max(maxWater, currWater);
+    // }
+    // }
+    // return maxWater;
+
+    // }
+    public static int calWater(ArrayList<Integer> height) {
+
+        int maxWater = 0;
+        int lp = 0;
+        int rp = height.size() - 1;
+
+        while (lp < rp) {
+            int ht = Math.min(height.get(lp), height.get(rp));
+            int width = rp - lp;
+
+            int currWater = ht * width;
+
+            maxWater = Math.max(maxWater, currWater);
+
+            if (height.get(lp) < height.get(rp)) {
+                lp++;
+            } else {
+                rp--;
+            }
+
+        }
+        return maxWater;
+    }
+
+    public static void main(String[] args) {
+        // initialiazation
+
+        ArrayList<Integer> height = new ArrayList<>();
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+        // System.out.println(calWater(height));
+        System.out.println(calWater(height));
+
+        // ArrayList<ArrayList<Integer>> mainlist = new ArrayList<>();
+
+        // ArrayList<Integer> list = new ArrayList<>();
+        // ArrayList<Integer> list2 = new ArrayList<>();
+        // ArrayList<Integer> list3 = new ArrayList<>();
+
+        // // insert values
+        // for (int i = 1; i <= 5; i++) {
+        // list.add(i * 1);
+        // list2.add(i * 2);
+        // list3.add(i * 3);
+        // }
+        // mainlist.add(list);
+        // mainlist.add(list2);
+        // mainlist.add(list3);
+        // System.out.println(mainlist);
+        // for (int i = 0; i < mainlist.size(); i++) {
+        // // System.out.print(mainlist.get(i));
+        // ArrayList<Integer> currList = mainlist.get(i);
+
+        // for (int j = 0; j< currList.size(); j++) {
+        // System.out.print(currList.get(j) + " ");
+        // }
+        // System.out.println();
+
+        // }
+
+        // list2.add(10);/
+
+        // int max = Integer.MIN_VALUE;
+        // int idx1 = 1;
+        // int idx2 = 4;
+        // System.out.println(list2);
+        // swap(list2, idx1, idx2);
+        // System.out.println(list2);
+        // //sorting in ascending order
+        // Collections.sort(list2);
+        // System.out.println(list2);
+
+        // // sorting in descending order
+        // Collections.sort(list2, Collections.reverseOrder());
+        // System.out.println(list2);
+
+        // // operation
+        // // 1. Add
+        // list.add(1);
+        // list.add(2);
+        // list.add(3);
+        // list.add(4);
+        // list.add(5);
+        // System.out.println(list);
+
+        // // 2.get
+        // System.out.println(list.get(0));
+
+        // // 3. remove
+        // System.out.println(list.remove(2));
+        // System.out.println(list);
+
+        // //4.set
+        // list.set(3, 10);
+        // System.out.println(list);
+
+        // //5.contain
+        // System.out.println(list.contains(10));
+        // System.out.println(list.contains(12));
+        // // special case
+        // list.add(0, 20);
+        // System.out.println(list);
+
+        // for (int i = list2.size() - 1; i >= 0; i--) {
+        // System.out.print(list2.get(i) + " ");
+
+        // if (max < list2.get(i)) {
+        // max = list2.get(i);
+        // }
+        // }
+        // System.out.println();
+        // System.out.println(max);
+
+        // }
+    }
+
+}
