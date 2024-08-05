f289c74925039c46b18b9a9faf577d5ad69681c5
Jay Bardhan Tiwari <103141320+JayBardhanTiwari@users.noreply.github.com>
2024-08-05 23:28:14 +0530

    Kth Distinct string in Array

---
+++ Leetcode/Distinct.java
@@ -0,0 +1,28 @@
+package Leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Distinct {
+    class Solution {
+        public String kthDistinct(String[] arr, int k) {
+            Map<String, Integer> counter = new HashMap<>();
+            for (String v : arr) {
+                counter.put(v, counter.getOrDefault(v, 0) + 1);
+            }
+            for (String v : arr) {
+                if (counter.get(v) == 1) {
+                    --k;
+                    if (k == 0) {
+                        return v;
+                    }
+                }
+            }
+            return "";
+        }
+
+        public static void main(String[] args) {
+
+        }
+    }
+}
